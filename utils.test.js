38248716344f667e50032306da5a03b016406fec
stdavis <stdavis@utah.gov>
2024-06-13 12:47:10 -0600

    fix: more consistent subsequent prerelease versions
    Fixes #106

--- utils.test.js
+++ utils.test.js
@@ -16,25 +16,28 @@ describe('getNewVersion', () => {
   // 0: last tag
   // 1: conventional recommended bump type
   // 2: prerelease
-  // 3: expected release type
+  // 3: last prod tag
+  // 4: expected release type
   const cases = [
-    ['1.0.0', 'patch', false, '1.0.1'],
-    ['1.0.0', 'patch', true, '1.0.1-0'],
-    ['1.0.1-0', 'patch', true, '1.0.1-1'],
-    ['1.0.0-0', 'patch', true, '1.0.0-1'],
-    ['1.0.1-0', 'minor', true, '1.1.0-0'],
-    ['1.0.1', 'minor', false, '1.1.0'],
-    ['1.0.1', 'major', false, '2.0.0'],
-    ['1.0.1', 'major', true, '2.0.0-0'],
-    ['1.0.1-0', 'major', true, '2.0.0-0'],
-    [null, 'minor', false, '1.0.0'],
-    [null, 'major', false, '1.0.0'],
-    [null, 'major', true, '1.0.0-0'],
-    ['2.0.0-0', 'minor', true, '2.0.0-1'],
+    ['1.0.0', 'patch', false, '1.0.0', '1.0.1'],
+    ['1.0.0', 'patch', true, '1.0.0', '1.0.1-0'],
+    ['1.0.1-0', 'patch', true, '1.0.1', '1.0.1-1'],
+    ['1.0.0-0', 'patch', true, '1.0.0', '1.0.0-1'],
+    ['1.0.1-0', 'minor', true, '1.0.1', '1.1.0-0'],
+    ['1.0.1', 'minor', false, '1.0.1', '1.1.0'],
+    ['1.0.1', 'major', false, '1.0.1', '2.0.0'],
+    ['1.0.1', 'major', true, '1.0.1', '2.0.0-0'],
+    ['1.0.1-0', 'major', true, '1.0.1', '2.0.0-0'],
+    [null, 'minor', false, null, '1.0.0'],
+    [null, 'major', false, null, '1.0.0'],
+    [null, 'major', true, null, '1.0.0-0'],
+    ['2.0.0-0', 'minor', true, '2.0.0', '2.0.0-1'],
+    ['1.3.0-7', 'minor', true, '1.2.6', '1.3.0-8'],
+    ['1.3.0-7', 'major', true, '1.2.6', '2.0.0-0'],
   ];
 
-  test.each(cases)('%s with %s and prerelease: %j should be %s', (lastTag, bumpType, prerelease, expectation) => {
-    expect(getNewVersion(lastTag, bumpType, prerelease)).toBe(expectation);
+  test.each(cases)('%s, %s, %j, %s => %s', (lastTag, bumpType, prerelease, lastProdTag, expectation) => {
+    expect(getNewVersion(lastTag, bumpType, prerelease, lastProdTag)).toBe(expectation);
   });
 });
 
