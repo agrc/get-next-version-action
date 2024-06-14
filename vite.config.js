b543ba2dfc4537c5f970dfe1c83902a6ba1c0fbf
stdavis <stdavis@utah.gov>
2024-06-14 10:11:20 -0600

    refactor: clean up the root directory

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
       cleanOnRerun: true,
       provider: 'v8',
       reporter: ['text', 'lcov'],
-      exclude: ['index.ts', 'cjs-shim.ts', 'dist/*'],
+      exclude: ['src/index.ts', 'support/cjs-shim.ts', 'dist/*'],
     },
   },
 });
