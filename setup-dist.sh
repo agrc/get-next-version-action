2bf13149ad9c62394c437521b2a337d37252ec7d
stdavis <stdavis@utah.gov>
2024-06-14 10:04:30 -0600

    chore: make sure that dist has a consistent preset version with src

--- setup-dist.sh
+++ setup-dist.sh
@@ -2,6 +2,7 @@
 # bundling it with esbuild or ncc did not work
 rm -rf dist
 mkdir dist
+VERSION=$(node -e 'console.log(require("./package.json").dependencies["conventional-changelog-angular"])')
 cd dist
 npm init es6 -y
-npm i conventional-changelog-angular
+npm i conventional-changelog-angular@$VERSION
